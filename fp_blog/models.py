58fce21f9d3b4da66c4d34420f6ce9612020b1cf
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-11-04 23:23:38 +0000

    Update models.py UserProfile, UserAction for personal finance planner issue #29

--- fp_blog/models.py
+++ fp_blog/models.py
@@ -68,48 +68,36 @@ class Action(models.Model):
     def __str__(self):
         return f"Action {self.action_seq} {self.action_desc} "
 
+
 #User profile model extending allauth user model
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    firstname = models.CharField(max_length=40, null=True, blank=True)
-    lastname = models.CharField(max_length=100, null=True, blank=True)
+    name = models.CharField(max_length=50, null=True, blank=True)
     profile_image = CloudinaryField('image', default='placeholder')
-    birth_year = IntgerField
-    age_approx = IntegerField
-    age_exact = IntegerField
+    birth_year = models.PositiveIntegerField()
+    age_approx = models.PositiveIntegerField()
+    age_exact = models.PositiveIntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now_add=True)
    
     def __str__(self):
-    if self.user:
-        return f"{self.first_name + ' ' + self.last_name}"
-    else:
-        return f"New Customer / Not Provided"
-
+        return f"Hi {self.name}"
 
 # Class UserAction, action that the user has copied from an article
-
-
-class Action(models.Model):
-    action_seq = models.IntegerField(default=10)
-    article = models.ForeignKey(Article, on_delete=models.CASCADE,
-                                related_name="user_actions")
+class UserAction(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_actions" )
+    user_action_seq = models.IntegerField(default=10)
+    parent_article = models.ManyToManyField(Article, related_name="article_personal_actions")
     action_desc = models.CharField(max_length=200, default='Action:  ')
-    author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="action_created_by")
+    action_taken = models.CharField(max_length=200, default='Taken:  ')
+    observation = models.CharField(max_length=200, default='Notes:  ')
+    completed = models.BooleanField(null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    completed_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['article', 'action_seq']
+        ordering = ['user_action_seq']
 
     def __str__(self):
         return f"Action {self.action_seq} {self.action_desc} "
 
-
-    article = models.ForeignKey(Article, on_delete=models.CASCADE,
-                                related_name="comments")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    approved = models.BooleanField(default=False)

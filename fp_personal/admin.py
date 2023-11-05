e9481c6748433d980973191055935b7ecd6f9781
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-11-05 22:11:47 +0000

    Add UserFavourite to models and test Issue #29

--- fp_personal/admin.py
+++ fp_personal/admin.py
@@ -1,26 +1,38 @@
 from django.contrib import admin
-from .models import Article
+from datetime import datetime
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Article
 from .models import User
 from .models import UserProfile
 from .models import UserAction
+from .models import UserFavourite
+
 
 # Register your models here.
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'name', 'birth_year', 'last_login')
-    search_fields = ['user', 'email']
+    list_display = ('user', 'birth_year', 'profile_image', 'created_on')
+    search_fields = ['user']
     
 
 @admin.register(UserAction)
 class ActionAdmin(SummernoteModelAdmin):
-    list_display = ('user', 'user_action_seq', 'parent_article', 'action_desc', 'action_taken', 
+    list_display = ('user', 'user_action_seq', 'parent_article', 'user_action_desc', 'user_action_taken', 
                     'observation', 'created_on', 'completed_on')
-    list_filter = ('user', 'user_action_seq', 'parent_article', 'created_on', 'completed')
+    list_filter = ('completed','user', 'user_action_seq', 'parent_article', 'created_on', )
     sortable_by = ['user', 'user_action_seq']
-    search_fields = ['user', 'action_desc']
+    search_fields = ['user', 'user_action_desc']
 
     actions = ['complete_actions']
 
     def complete_actions(self, request, queryset):
-        queryset.update(completed=True)
+        queryset.update(completed=True, completed_on=datetime.now())
+    
+    
+@admin.register(UserFavourite)
+class FavouriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'favourite_article', 'created_on')
+    list_filter = ('created_on', 'user', 'favourite_article')
+    sortable_by = ['user', 'favourite_article']
+    search_fields = ['user', 'favourite_article']
+        
--- fp_personal/models.py
+++ fp_personal/models.py
@@ -8,31 +8,41 @@ from fp_blog.models import Action
 #User profile model extending allauth user model
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50, null=True, blank=True)
     profile_image = CloudinaryField('image', default='placeholder')
     birth_year = models.PositiveIntegerField()
     age_approx = models.PositiveIntegerField()
     age_exact = models.PositiveIntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now_add=True)
-   
+       
     def __str__(self):
-        return f"Hi {self.name}"
+        return f"User: {self.user.first_name} {self.user.last_name},  UserId: {self.user},  Email: {self.user.email},  Last login: {self.user.last_login.day}/{self.user.last_login.month}/{self.user.last_login.year}"
 
 # Class UserAction, action that the user has copied from an article
 class UserAction(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_actions" )
     user_action_seq = models.IntegerField(default=10)
     parent_article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="article_personal_actions")
-    action_desc = models.CharField(max_length=200, default='Action:  ')
-    action_taken = models.CharField(max_length=200, default='Taken:  ')
+    user_action_desc = models.CharField(max_length=200, default='Action:  ')
+    user_action_taken = models.CharField(max_length=200, default='Taken:  ')
     observation = models.CharField(max_length=200, default='Notes:  ')
     completed = models.BooleanField(null=False, blank=False)
     created_on = models.DateTimeField(auto_now_add=True)
-    completed_on = models.DateTimeField(auto_now_add=True)
+    completed_on = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         ordering = ['user', 'user_action_seq']
 
     def __str__(self):
-        return f"Action {self.action_seq} {self.action_desc} "
+        return f"Action {self.user_action_seq} {self.user_action_desc} "
+
+# Class UserFavourite, a set of Favourite Articles for the user
+class UserFavourite(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_favourite" )
+    favourite_article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="article_favourite")
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['user', 'created_on']
+
+    def __str__(self):
+        return f"User {self.user} {self.favourite_article} "

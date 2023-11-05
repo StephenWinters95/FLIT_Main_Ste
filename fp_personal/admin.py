97d051c78534c35d766a00c0ce76bcf81d69450b
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-11-05 18:39:37 +0000

    Setup model.py and admin.py in app fp_personal Issue #29

--- fp_personal/admin.py
+++ fp_personal/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Article
+from django_summernote.admin import SummernoteModelAdmin
+from .models import User
+from .models import UserProfile
+from .models import UserAction
 
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'birth_year', 'last_login')
+    search_fields = ['user', 'email']
+    
+
+@admin.register(UserAction)
+class ActionAdmin(SummernoteModelAdmin):
+    list_display = ('user', 'user_action_seq', 'parent_article', 'action_desc', 'action_taken', 
+                    'observation', 'created_on', 'completed_on')
+    list_filter = ('user', 'user_action_seq', 'parent_article', 'created_on', 'completed')
+    sortable_by = ['user', 'user_action_seq']
+    search_fields = ['user', 'action_desc']
+
+    actions = ['complete_actions']
+
+    def complete_actions(self, request, queryset):
+        queryset.update(completed=True)
--- fp_personal/models.py
+++ fp_personal/models.py
@@ -5,3 +5,34 @@ from fp_blog.models import Article
 from fp_blog.models import Action
 
 # Create your models here.
+#User profile model extending allauth user model
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    profile_image = CloudinaryField('image', default='placeholder')
+    birth_year = models.PositiveIntegerField()
+    age_approx = models.PositiveIntegerField()
+    age_exact = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+   
+    def __str__(self):
+        return f"Hi {self.name}"
+
+# Class UserAction, action that the user has copied from an article
+class UserAction(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_actions" )
+    user_action_seq = models.IntegerField(default=10)
+    parent_article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="article_personal_actions")
+    action_desc = models.CharField(max_length=200, default='Action:  ')
+    action_taken = models.CharField(max_length=200, default='Taken:  ')
+    observation = models.CharField(max_length=200, default='Notes:  ')
+    completed = models.BooleanField(null=False, blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    completed_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['user', 'user_action_seq']
+
+    def __str__(self):
+        return f"Action {self.action_seq} {self.action_desc} "
